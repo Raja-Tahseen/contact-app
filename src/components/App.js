e5e1ab59fa7b1dca0edc64a0929f094c8936bc3b
Raja Tahseen <rajatahseen86@gmail.com>
2024-01-31 03:24:32 +0500

    Add Functional components(Header.js,ContactList.js), Add Class component(AddContact.js)

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header.js";
+import AddContact from "./AddContact.js";
+import ContactList from "./ContactList.js";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ContactList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactList = () => {
+  return <div className="ui cell list">Contact List</div>;
+};
+
+export default ContactList;
