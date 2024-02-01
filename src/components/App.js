ed6a638b649bb786a53bdf6037ce0436317fadbb
Raja Tahseen <rajatahseen86@gmail.com>
2024-02-02 00:24:20 +0500

    Store and retrieve data from browser local storage on page refresh.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header.js";
 import AddContact from "./AddContact.js";
 import ContactList from "./ContactList.js";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
   };
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY, LOCAL_STORAGE_KEY)
+    );
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
