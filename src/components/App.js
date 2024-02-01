cb4a5e19bedc42ec612ca4b859fad11b9efdd722
Raja Tahseen <rajatahseen86@gmail.com>
2024-02-01 23:40:44 +0500

    Make contacts lists dynamic, Submit form, make use of React hook -> 'useState'

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header.js";
 import AddContact from "./AddContact.js";
 import ContactList from "./ContactList.js";
 
 function App() {
-  const contacts = [
-    { id: "1", name: "Tahseen", email: "rajatahseen@gmail.com" },
-    { id: "2", name: "Akhtar", email: "rajaakhtar@gmail.com" },
-  ];
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} />
       {/* props- pass data {contacts} from parent to child */}
     </div>
