247d712d438f670164ecb37930f436c7aa5964b5
Raja Tahseen <rajatahseen86@gmail.com>
2024-02-02 03:04:04 +0500

    Define handlers and props in 3 layered parents,childs and pass id value from child to parent and to parent. Update index.js to remove warning and saving of list in local storage.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 import "./App.css";
 import Header from "./Header.js";
 import AddContact from "./AddContact.js";
@@ -6,19 +7,29 @@ import ContactList from "./ContactList.js";
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
 
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    // setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuid(), ...contact }]); //---Need a add a uniue Id so created new object
   };
 
-  useEffect(() => {
-    const retrieveContacts = JSON.parse(
-      localStorage.getItem(LOCAL_STORAGE_KEY, LOCAL_STORAGE_KEY)
-    );
-    if (retrieveContacts) setContacts(retrieveContacts);
-  }, []);
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
+  };
+
+  // useEffect(() => {
+  //   const retrieveContacts = JSON.parse(
+  //     localStorage.getItem(LOCAL_STORAGE_KEY, LOCAL_STORAGE_KEY)
+  //   );
+  //   if (retrieveContacts) setContacts(retrieveContacts);
+  // }, []);
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
@@ -28,7 +39,7 @@ function App() {
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
       {/* props- pass data {contacts} from parent to child */}
     </div>
   );
